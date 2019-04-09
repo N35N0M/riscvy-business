fdcb3d7d2970c04a67c37ec30b3a66f035e9647a
n35n0m <krismonsen@icloud.com>
2019-04-09 21:49:38 +0200

    Rewrites the picorv to FreeAHB adapter to properly handle wstrb, and to convert directly to FreeAHB UI. Also includes TBs which verify only the freeahb adapter, and one which verifies the freeahb adapter and a freeahb instance. Verification seems to indicate that they function for intended use.

---
+++ riscvy-grlib/lib/riscv/picorv/adapter_and_freeahb_tb.sv
@@ -0,0 +1,143 @@
+// Inspired by the style of the FreeAHB TB.
+
+// DUV is the FreeAHB Adapter.
+// We will apply valid stimulus from the Pico IF and FreeAHB master manually.
+module adapter_and_freeahb_test;
+    // ADAPTER
+    logic 			[31:0]		freeahb_wdata;
+    logic 								freeahb_valid;
+   	logic		    [31:0]		freeahb_addr;
+    logic       [2:0]			freeahb_size;
+ 		logic                 freeahb_write;
+    logic 								freeahb_read;
+    logic 			[31:0]		freeahb_min_len;// Minimum "guaranteed size of burst"
+    logic 								freeahb_cont;			// Continues prev transfer
+    logic 			[3:0]			freeahb_prot;
+    logic 								freeahb_lock;
+
+    logic 			   				freeahb_next; // Asserted indicates transfer finished.
+    logic 			  [31:0]	freeahb_rdata;
+    logic         [31:0]  freeahb_result_addr; // Not used.
+    logic 				  			freeahb_ready; 				// rdata contains valid data.
+
+    // Native PicoRV32 memory interface
+
+    logic 			[31:0] 		mem_rdata;
+    logic         				mem_ready;
+    bit   			[31:0] 		mem_addr;
+    bit   			[31:0] 		mem_wdata;
+    bit   			[3:0] 		mem_wstrb;
+    bit            				mem_valid;
+    bit            				mem_instr;
+
+    // FREEAHB
+
+    bit                  i_hgrant;
+    bit [31:0]           i_hrdata;
+    bit                  i_hready;
+    bit[1:0]             i_hresp;
+
+    logic [31:0]           o_haddr;
+    logic [2:0]            o_hburst;
+    logic [2:0]            o_hsize;
+    logic [1:0]            o_htrans;
+    logic[31:0]            o_hwdata;
+    logic                  o_hwrite;
+    logic                  o_hbusreq;
+
+    logic [3:0]            o_hprot;
+    logic                  o_hlock;
+
+
+    bit clk;
+    bit resetn;
+
+ahb_master #(.DATA_WDT(32), .BEAT_WDT(32)) FREEAHB_MAST (.i_hclk(clk),
+                                                         .i_hreset_n(resetn),
+                                                         .i_data(freeahb_wdata),
+                                                         .i_valid(freeahb_valid),
+                                                         .i_addr(freeahb_addr),
+                                                         .i_size(freeahb_size),
+                                                         .i_write(freeahb_write),
+                                                         .i_read(freeahb_read),
+                                                         .i_min_len(freeahb_min_len),
+                                                         .i_cont(freeahb_cont),
+                                                         .i_prot(freeahb_prot),
+                                                         .i_lock(freeahb_lock),
+                                                         .o_next(freeahb_next),
+                                                         .o_data(freeahb_rdata),
+                                                         .o_addr(freeahb_result_addr),
+                                                         .o_ready(freeahb_ready),
+                                                         .*);
+
+picorv32_to_freeahb_adapter FREEAHB_ADAPT    (.*);
+
+
+always #10 clk++;
+
+
+initial
+begin
+        $dumpfile("freeahb_adapter.vcd");
+        $dumpvars;
+        resetn <= 1;
+        d(10);
+        // FIRST CASE: TEST READ
+        // Initialize write session by acting as PicoRV memory IF
+        mem_addr    <= 32'b10000000000000000000000000000000;
+        mem_wdata   <= 32'hF0FF0FAA;
+        mem_wstrb   <= 4'b1111;
+        mem_valid   <= 1'b1;
+        mem_instr   <= 1'b1;
+
+        wait_for_hbusreq;
+
+        // We grant it the bus and simulate zero wait state conditions.
+        i_hgrant <= 1'b1;
+        i_hready <= 1'b1;
+        i_hresp  <= 2'b00; // Slave responds OKAY.
+
+        // Let the write transfer finish.
+        wait_for_if_ready;
+
+        $finish;
+end
+
+task wait_for_hbusreq;
+        d(1);
+        while(o_hbusreq !== 1)
+        begin
+                d(1);
+        end
+endtask
+
+task wait_for_freeahb_valid;
+        d(1);
+        while(freeahb_valid !== 1)
+        begin
+                d(1);
+        end
+endtask
+
+task wait_for_if_ready;
+        d(1);
+        while(mem_ready !== 1)
+        begin
+                d(1);
+        end
+endtask
+
+task wait_for_next;
+        d(1);
+        while(freeahb_next !== 1)
+        begin
+                d(1);
+        end
+endtask
+
+task d(int x);
+        repeat(x)
+        @(posedge clk);
+endtask
+
+endmodule
---
+++ riscvy-grlib/lib/riscv/picorv/picorv32_freeahb_adapter.v
@@ -0,0 +1,135 @@
+// *****************************************************************************
+// picorv32_to_freeahb_adapter
+// *****************************************************************************
+
+module picorv32_to_freeahb_adapter (
+	input clk, resetn,												// Will be provided by AHB bus.
+
+  // FreeAHB interface
+  output reg			[31:0]		freeahb_wdata,
+  output reg								freeahb_valid,
+	output reg			[31:0]		freeahb_addr,
+	output reg     [2:0]			freeahb_size,
+	output reg								freeahb_write,
+	output reg								freeahb_read,
+	output reg			[31:0]		freeahb_min_len,// Minimum "guaranteed size of burst"
+	output reg								freeahb_cont, 			// Continues prev transfer
+  output reg			[3:0]			freeahb_prot,
+	output reg								freeahb_lock,
+
+	input 								freeahb_next, // Asserted indicates transfer finished.
+	input 			[31:0]		freeahb_rdata,
+	input       [31:0]    freeahb_result_addr, // Not used.
+	input 								freeahb_ready, 				// rdata contains valid data.
+
+	// Native PicoRV32 memory interface
+	input         				mem_valid,
+	input         				mem_instr,
+	output reg       				mem_ready,
+	input  			[31:0] 		mem_addr,
+	input  			[31:0] 		mem_wdata,
+	input  			[3:0] 		mem_wstrb,
+	output 			[31:0] 		mem_rdata
+);
+
+
+	assign mem_rdata = freeahb_rdata;
+
+
+	reg [3:0] write_ctr;	// Used to keep track of which bit in
+												// wstrb we are working on.
+
+	always @(posedge clk or negedge resetn) begin
+	  // Idle/reset conditions.
+		// We expect that a finished transfer (mem_ready) always lowers mem_valid.
+		if (!resetn || !mem_valid) begin
+			freeahb_valid <= 0;
+			mem_ready 		<= 0;
+			write_ctr 		<= 0;
+		end
+
+		// *************************************************************************
+		// READS
+		//**************************************************************************
+		// READ transfer start
+		else if (mem_wstrb == 4'b0000 && !freeahb_valid) begin
+			freeahb_wdata 			<= 0;
+			freeahb_valid 			<= mem_valid;
+			freeahb_addr  			<= mem_addr;
+			freeahb_size  			<= 3'b010;
+			freeahb_write 			<= 1'b0;
+			freeahb_read  			<= 1'b1;
+			freeahb_min_len 	  <= 32;
+			freeahb_cont				<= 1'b0;
+			freeahb_prot				<= mem_instr ? 4'b0000 : 4'b0001;
+			freeahb_lock				<= 1'b1;
+		end
+
+		// READ transfer complete
+		else if (mem_wstrb == 4'b0000 && freeahb_valid && freeahb_ready) begin
+			mem_ready <= 1'b1;
+		end
+
+		// *************************************************************************
+		// WRITES
+		// *************************************************************************
+
+    // WRITE sequences
+		// The mem IF outputs WSTRB (write strobes), but this is a AXI4 construct,
+		// and does not suit AHB. We therefore have to translate the strobes to
+		// individual AHB transfers.
+		// There is room for improvement, such as combining sequential strobes
+		// into one transfer, to reduce transfers from bestcase 4clk to bestcase
+		// 1 clk (all bytes enabled
+		else if (mem_wstrb != 4'b0000 && freeahb_next && write_ctr < 4) begin
+		  if (mem_wstrb[3-write_ctr] === 1) begin
+				case (3-write_ctr)
+					3:	begin
+								freeahb_wdata 			<= mem_wdata[31:24];
+								freeahb_addr  			<= mem_addr;
+
+							end
+					2: begin
+								freeahb_wdata 			<= mem_wdata[23:16];
+								freeahb_addr  			<= mem_addr + 1;
+
+						 end
+					1:
+						 begin
+						 		freeahb_wdata 			<= mem_wdata[15:8];
+						 		freeahb_addr  			<= mem_addr + 2;
+						 end
+					0:
+	 					 begin
+						 		freeahb_wdata 			<= mem_wdata[7:0];
+						 		freeahb_addr  			<= mem_addr + 3;
+	 					 end
+				endcase
+
+				freeahb_valid 			<= mem_valid;
+				freeahb_size  			<= 3'b000; // byte
+				freeahb_write 			<= 1'b1;
+				freeahb_read  			<= 1'b0;
+				freeahb_min_len 		<= 8;
+				freeahb_cont				<= 1'b0;	// Byte strobes are not necessarily
+																			// sequential. Start new transfer.
+				freeahb_prot				<= mem_instr ? 4'b0000 : 4'b0001;
+				freeahb_lock				<= 1'b1;  // We always lock to ensure that the
+																			// entire write is done noninterrupted.
+
+			end
+			write_ctr <= write_ctr + 1;
+		end
+
+		// If we couldnt start the next write, it can be because
+		// FreeAHB doesnt have the bus. Setting i_write to 1 will do.
+		else if (mem_wstrb != 4'b0000 && write_ctr < 4) begin
+			freeahb_write <= 1;
+		end
+
+		// Write sequence finished
+		else if (mem_wstrb != 4'b0000 && freeahb_next && write_ctr == 4)
+			mem_ready <= 1'b1;
+		end
+
+endmodule
